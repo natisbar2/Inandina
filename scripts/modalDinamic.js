586e19638f10e138f52b083989884b1247a2adac
natisbar <48846184+natisbar@users.noreply.github.com>
2021-07-25 22:50:11 -0500

    Update modalDinamic.js

--- scripts/modalDinamic.js
+++ scripts/modalDinamic.js
@@ -1,7 +1,7 @@
 function showModal(title, content, close_x, button){
     let modalHeader = `<div class="modal micromodal-slide" id="modal-1" aria-hidden="true"><div class="modal__overlay" tabindex="-1" data-micromodal-close>
       <div class="modal__container" role="dialog" aria-modal="true" aria-labelledby="modal-1-title">
-          <header class="modal__header centerDiv" >
+          <header class="centerDiv" >
                   <h2 class="centerDiv">
                     ${title}
                   </h2>`;
@@ -36,4 +36,4 @@ function showModal(title, content, close_x, button){
     }
   }              
   
-//   class="modal__title"
+//   class="modal__title"    class="modal__header
