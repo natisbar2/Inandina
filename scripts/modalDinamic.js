a1c14c6bd54f46546157e791ea86a2ed32887e64
natisbar <48846184+natisbar@users.noreply.github.com>
2021-07-25 21:56:15 -0500

    Create modalDinamic.js

---
+++ scripts/modalDinamic.js
@@ -0,0 +1,38 @@
+function showModal(title, content, close_x, button){
+    let modalHeader = `<div class="modal micromodal-slide" id="modal-1" aria-hidden="true"><div class="modal__overlay" tabindex="-1" data-micromodal-close>
+      <div class="modal__container" role="dialog" aria-modal="true" aria-labelledby="modal-1-title">
+          <header class="modal__header">
+                  <h2 class="modal__title">
+                    ${title}
+                  </h2>`;
+              
+    let modalFirstClose = `<button class="modal__close" aria-label="Close modal" data-custom-close="modal-1" ></button>`;
+    let modalContent = `</header>
+          <div class="modal-content-content">
+              <div class="modal__content centerDiv">
+                <p>${content}</p>
+              </div>
+          </div>`;
+    let modalFooter = `<footer class="modal__footer">
+                  <div class="centerDiv">
+                  <button class="modal__btn" data-custom-close="modal-1"
+                      aria-label="Close this dialog window" id="modalClose">Close</button>
+                  </div>
+              </footer>`;
+    let modalCierre = `</div></div></div>`;
+              
+    // let a = document.getElementById("modal-1");
+    text = modalHeader;
+    if (close_x){
+      text += modalFirstClose;
+    }
+    text += modalContent;
+    if (button){
+      text += modalFooter;
+    }
+    text += modalCierre;
+    if(!document.getElementById("modal-1")){
+      document.body.innerHTML += document.body.innerHTML;
+    }
+  }              
+  
